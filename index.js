62cd4139c6bd07a940c4e7b2b25818653c35febd
Rezaul Kamrim Nafiz <md344358@gamil.com>
2021-08-14 18:21:24 +0600

    completely push data to database and kickOut fakeData

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 const port = 4000
 app.get('/', (req, res) => {
-  res.send('Hello Node')
+    res.send('Hello Node')
 })
 
 //use cors and body-parser
@@ -21,21 +21,46 @@ const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0mhgi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const ProductsCollection = client.db("recapemajhon").collection("Productss");
-  //create a post first from app//
-  app.post('/addProduct', (req, res) => {
-      const Products = req.body;
-      
-    ProductsCollection.insertMany(Products)
-    .then(result => {
-        res.send(result.insertedIds)
-        console.log(result.insertedIds)
+    const productsCollection = client.db("recapemajhon").collection("Products");
+
+    //load all data form fakeData to database //complete C of the CRUD//
+    app.post('/addProduct', (req, res) => {
+        const products = req.body;
+        productsCollection.insertMany(products)
+            .then(result => {
+                res.send(result.insertedIds)
+                console.log(result.insertedIds)
+            })
     })
-  })
+
+    //upload/Read a product Item form the database// complete R of th CRUD//
+    app.get('/products', (req, res) => {
+        productsCollection.find({})//get product from database//then convert it to array//then send it to documents//
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
+    })
+
+    //load Single Products//
+    app.get('/product/:key', (req, res) => {
+        productsCollection.find({key: req.params.key})//find single product by "using req.params.key" //
+        .toArray( (err, documents) => {
+            res.send(documents[0]);
+        })
+    })
+
+    //get/load product for review components//find by productKeys//
+    app.post('/productsByKeys', (req, res) => {
+        const productKeys = req.body;
+        productsCollection.find({key: {$in: productKeys}})
+        .toArray( (err, documents) => {
+            res.send(documents)
+        })
+    })
 });
 
 
 //from express
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
